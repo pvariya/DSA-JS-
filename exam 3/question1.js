ef68613b0c6852270b216f0c33c3c45c222a0499
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-08-20 11:31:14 +0530

    Update question1.js

--- exam 3/question1.js
+++ exam 3/question1.js
@@ -1,17 +1,30 @@
 let str1 = 'listen', str2 = 'silent'
-let sortStr1 = str1.split('').sort()
-let sortStr2 = str2.split('').sort()
 
 
-for (let i = 0; i < str1.length; i++) {
-    if (sortStr1[i] !== sortStr2[i]) {
-        console.log("false")
-        break;
+const Anagrams = (str1, str2) => {
+
+    let sortStr1 = str1.split('').sort().join('')
+    let sortStr2 = str2.split('').sort().join('')
+
+    if (sortStr1 === sortStr2) {
+        return true
     }
     else {
-        console.log("true")
-        break
+        return false
     }
+
+
+    // for (let i = 0; i < str1.length; i++) {
+    //     for (let j = 0; j < str2.length; j++) {
+    //         if (sortStr1[i] === sortStr2[i]) {
+    //             console.log( sortStr1[i] , sortStr2[i]);
+    //             // return true
+    //         }
+    //         // else {
+    //         //     return false
+    //         // }
+    //     }
+    // }
 }
 
-console.log();
+console.log(Anagrams(str1, str2))
